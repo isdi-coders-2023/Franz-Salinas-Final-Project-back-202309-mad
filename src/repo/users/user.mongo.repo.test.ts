77307e25b3f594fc1a58cbb9d64e27b18d94f684
Franz <fernandofranquiz@gmail.com>
2023-12-05 21:31:23 +0100

    Again trying to fix sonar

---
+++ src/repo/users/user.mongo.repo.test.ts
@@ -0,0 +1,67 @@
+import { UserMongoRepo } from './user.mongo.repo.js';
+import { Auth } from '../../services/auth.js';
+import { LoginUser, User } from '../../entities/users.js';
+import { UserModel } from './users.mongo.model.js';
+
+jest.mock('./users.mongo.model.js');
+jest.mock('../../services/auth.js');
+
+describe('GivenUserMongoRepo', () => {
+  Auth.hast = jest.fn();
+  Auth.compare = jest.fn().mockResolvedValue(true);
+  let repo: UserMongoRepo;
+  describe('When we instantiate it without errors', () => {
+    const exec = jest.fn().mockResolvedValue('Test');
+    beforeEach(() => {
+      const mockQueryMethod = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+        exec,
+      });
+      UserModel.find = mockQueryMethod;
+      UserModel.findById = mockQueryMethod;
+      UserModel.findOne = mockQueryMethod;
+      UserModel.findByIdAndUpdate = mockQueryMethod;
+      UserModel.findByIdAndDelete = mockQueryMethod;
+      UserModel.create = jest.fn().mockResolvedValue('Test');
+      repo = new UserMongoRepo();
+    });
+
+    test('Then it should execute create', async () => {
+      const result = await repo.create({} as Omit<User, 'id'>);
+      expect(Auth.hast).toHaveBeenCalled();
+      expect(UserModel.create).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute login', async () => {
+      const result = await repo.login({ email: '' } as LoginUser);
+      expect(UserModel.findOne).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute getAll', async () => {
+      const result = await repo.getAll();
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute getById', async () => {
+      const result = await repo.getById('');
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute update', async () => {
+      const result = await repo.update('1', { id: '2' });
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute delete', async () => {
+      await repo.delete('1');
+      expect(exec).toHaveBeenCalled();
+    });
+  });
+});
